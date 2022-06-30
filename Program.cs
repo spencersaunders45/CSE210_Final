1a5920872fdad357ce914ba5314ca7b2ad5f7f43
Jakabob247 <jakabob2470@gmail.com>
2022-06-30 11:31:16 -0600

    Stuff

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace CSE210_Final;
+﻿using System.Numerics;
+using CSE210_Final.Casting;
+using CSE210_Final.Directing;
+using CSE210_Final.Scripting;
+using CSE210_Final.Services;
+
+namespace CSE210_Final;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
         //TODO: Initialize game entry point here.
-        Console.WriteLine("Started!");
+        IServiceFactory serviceFactory = new RaylibServiceFactory();
+        Scene testScene = new Scene();
+
+        //-------------------Testing-------------------
+        serviceFactory.GetVideoService().SetBackground(Color.Black());
+        PlayerController playerController = new PlayerController(Vector2.Zero, Vector2.One * 5, Color.Green());
+        testScene.AddActor("player" , playerController);
+        //-------------------End testing-------------------
+        
+        Director director = new Director(serviceFactory);
+        director.Direct(testScene);
     }
 }

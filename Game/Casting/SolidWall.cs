577ce134ee23ec221dab8ee8aa1b9a272d529882
Jakabob <jakabob2470@gmail.com>
2022-07-06 17:52:58 -0600

    Starting implementation of player colliding with a wall. Not fully functional quite yet.

---
+++ Game/Casting/SolidWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+using CSE210_Final.Game.Scripting;
+using CSE210_Final.Game.Services;
+
+namespace CSE210_Final.Game.Casting;
+
+public class SolidWall : Actor
+{
+    public SolidWall(Vector2 pos, Vector2 size, Color color, Scene scene)
+    {
+        MoveTo(pos);
+        SizeTo(size);
+        Tint(color);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,21 +10,30 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        //TODO: Initialize game entry point here.
         IServiceFactory serviceFactory = new RaylibServiceFactory();
         Scene testScene = new Scene();
 
         //-------------------Testing-------------------
         serviceFactory.GetVideoService().SetBackground(Color.Black());
-        PlayerController playerController = new PlayerController(Vector2.One * 100, Vector2.One * 30, Color.Green());
+        PlayerController playerController = new PlayerController(Vector2.One * 100, Vector2.One * 30, Color.Green(), testScene);
         testScene.AddActor("player" , playerController);
+
+        for (int i = 0; i < 4; i++)
+        {
+            testScene.AddActor("wall", new SolidWall(new Vector2((i * 32) + 100, 200), Vector2.One * 32, Color.Purple(), testScene));
+        }
+        
         //-------------------End testing-------------------
 
+        // Update
         UpdateActorsAction updateActorsAction = new UpdateActorsAction(serviceFactory);
         testScene.AddAction(Phase.Input, updateActorsAction);
+        
+        // Draw
         DrawActorsAction drawActorsAction = new DrawActorsAction(serviceFactory);
         testScene.AddAction(Phase.Output ,drawActorsAction);
         
+        // Start Game
         Director director = new Director(serviceFactory);
         director.Direct(testScene);
     }

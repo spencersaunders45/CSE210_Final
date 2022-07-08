55c2894abee64ffab26aa9d1c4d8e2aba4cf9f3e
Spencer Saunders <spencer.saunders45@gmail.com>
2022-07-08 15:56:53 -0600

    added health attribute

--- Game/Casting/Skeleton.cs
+++ Game/Casting/Skeleton.cs
@@ -6,11 +6,12 @@ namespace CSE210_Final.Game.Casting
    class Skeleton : Actor
    {
 
-      public Skeleton(float x, float y , Vector2 size, Color color)
+      public Skeleton(float x, float y , Vector2 size, Color color, int health)
       {
          Tint(color);
          MoveTo(x, y);
          SizeTo(size);
+         _health = health;
       }
    }
 }

611283ff09af8175e50593e446ba668b19484ca9
spencersaunders45 <spencer.saunders45@gmail.com>
2022-07-16 20:30:43 -0600

    organized GameSceneLoader code

--- Game/Scripting/GameSceneLoader.cs
+++ Game/Scripting/GameSceneLoader.cs
@@ -24,32 +24,60 @@ public class GameSceneLoader : SceneLoader
         TitleSceneLoader titleSceneLoader = new TitleSceneLoader(serviceFactory);
         Image playerImage = new Image();
         PlayMusicAction playMusicAction = new PlayMusicAction(serviceFactory);
+        Actor camera = CreateCamera(playerImage);
+        Skeleton boss = new Skeleton(1070, 286, Vector2.One * 24, Color.Red(), 16, scene, serviceFactory, true);
 
-        DrawImageAction drawImageAction = new DrawImageAction(serviceFactory);
-        
-        
-        
         // Define Actions
+        DrawImageAction drawImageAction = new DrawImageAction(serviceFactory);
         UpdateActorsAction updateActorsAction = new UpdateActorsAction(serviceFactory);
         SceneTransitionAction sceneTransitionAction =
-            new SceneTransitionAction(serviceFactory, titleSceneLoader, KeyboardKey.Enter);
+        new SceneTransitionAction(serviceFactory, titleSceneLoader, KeyboardKey.Enter);
         DrawActorsAction drawActorsAction = new DrawActorsAction(serviceFactory);
         AnimatePlayerAction animatePlayerAction = new AnimatePlayerAction(serviceFactory, playerController, playerImage);
         DrawSkeletonAction drawSkeletonAction = new DrawSkeletonAction(serviceFactory);
-        // Label back = new Label();
+        SkeletonHandler skeletonHandler = new SkeletonHandler();
 
+        //Title screen background
+        Image background = CreateTitleScreen();
+        scene.Clear();
+
+        // Add Actors
+        scene.AddActor("background", background);
+        scene.AddActor("player-image", playerImage);
+        scene.AddActor("player" , playerController);
+        scene.AddActor("camera", camera);
+        scene.AddActor("boss" , boss);
+
+        //Populate level w/ walls
+        // 0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 41, 42, 43, 44, 45, 50, 51, 52, 53, 54, 55
+        CSVReader csvReader = new CSVReader();
+        CreateWalls(scene, csvReader);
+        
+        //Create Skeleton Spawns
+        CreateSkeletonSpawns(scene, csvReader, serviceFactory);
+
+        // Add Actions
+        scene.AddAction(Phase.Update, updateActorsAction);
+        scene.AddAction(Phase.Input, sceneTransitionAction);
+        scene.AddAction(Phase.Output, playMusicAction);
+        scene.AddAction(Phase.Output, drawActorsAction);
+        scene.AddAction(Phase.Output, animatePlayerAction);
+        scene.AddAction(Phase.Output, drawSkeletonAction);
+        scene.AddAction(Phase.Update, skeletonHandler);
+    }
+
+    private Image CreateTitleScreen()
+    {
         Image background = new Image();
         background.SizeTo(1280, 960);
         background.MoveTo(0, 0);
         background.Display("Assets/Images/background/IMAGE_SaveYourDog_Map.png");
+        return background;
+    }
 
-        // back.Display("press 'enter' to go back to main screen");
-        // back.MoveTo(0, 0);
-        // back.Align(Label.Left);
 
-        scene.Clear();
-
-        // Add Actors
+    private Actor CreateCamera(Image playerImage)
+    {
         Actor screen = new Actor();
         screen.SizeTo(640, 480);
         screen.MoveTo(0, 0);
@@ -58,26 +86,14 @@ public class GameSceneLoader : SceneLoader
         world.SizeTo(1280, 960);
         world.MoveTo(0, 0);
         Camera camera = new Camera(playerImage, screen, world);
+        return camera;
+    }
 
-        scene.AddActor("background", background);
 
-
-        scene.AddActor("player-image", playerImage);
-        scene.AddActor("player" , playerController);
-        // scene.AddActor("label", back);
-        scene.AddActor("camera", camera);
-
+    private void CreateWalls(Scene scene, CSVReader csvReader)
+    {
         
-        // Add Skeletons
-        Skeleton boss = new Skeleton(1070, 286, Vector2.One * 24, Color.Red(), 16, scene, serviceFactory, true);
-        scene.AddActor("boss" , boss);
-        
-        //Populate level w/ walls
-        // 0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 41, 42, 43, 44, 45, 50, 51, 52, 53, 54, 55
-        CSVReader csvReader= new CSVReader();
-
         string[][] map = csvReader.ReadCSV("Assets/Tiled/CSV_SaveYourDog_Tile Layer 1.csv");
-        string[][] skeletonSpawns = csvReader.ReadCSV("Assets/Tiled/CSV_SaveYourDog_Tile Layer 2.csv");
         string[] wallChecks = new[]
         {
             "0", "1", "2", "3", "4", "5", "10", "15", "20", "25", "30", "35", "40", "41", "42",
@@ -93,7 +109,12 @@ public class GameSceneLoader : SceneLoader
                 }
             }
         }
+    }
 
+
+    private void CreateSkeletonSpawns(Scene scene, CSVReader csvReader, IServiceFactory serviceFactory)
+    {
+        string[][] skeletonSpawns = csvReader.ReadCSV("Assets/Tiled/CSV_SaveYourDog_Tile Layer 2.csv");
         for (int x = 0; x < skeletonSpawns.Length; x++)
         {
             for (int y = 0; y < skeletonSpawns[x].Length; y++)
@@ -105,17 +126,6 @@ public class GameSceneLoader : SceneLoader
                 }
             }
         }
-
-        SkeletonHandler skeletonHandler = new SkeletonHandler();
-        
-        // Add Actions
-        scene.AddAction(Phase.Update, updateActorsAction);
-        scene.AddAction(Phase.Input, sceneTransitionAction);
-        scene.AddAction(Phase.Output, playMusicAction);
-        scene.AddAction(Phase.Output, drawActorsAction);
-        scene.AddAction(Phase.Output, animatePlayerAction);
-        scene.AddAction(Phase.Output, drawSkeletonAction);
-        scene.AddAction(Phase.Update, skeletonHandler);
     }
 
 }
